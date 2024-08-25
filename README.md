f1ecb7fc581c4bc90ffde431dfc2644c9fd29829
QentiX <programmisteconomist21@gmail.com>
2024-08-25 16:03:28 +0300

    create: added slider animation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # abstract-fire
-Beautiful animated full page slider using HTML, CSS, JS, GSAP Power
+Beautiful animated full page slider using HTML, CSS, JS, GSAP Power.
+Without adaptive!
+
+![alt text](/img/cover.png)
--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const video = document.querySelector('.video-background')
+
 const swiperText = new Swiper('.swiper', {
 	speed: 1600,
 	mousewheel: {},
@@ -10,3 +12,20 @@ const swiperText = new Swiper('.swiper', {
 		nextEl: '.swiper-button-next',
 	},
 })
+
+// Привязка к событию смены слайдов
+swiperText.on('slideChange', function () {
+	gsap.to(video, 1.6, {
+		currentTime: (video.duration / (this.slides.length - 1)) * this.realIndex,
+		ease: Power2.easeOut,
+	})
+})
+
+// Уменьшение яркости огня при активном слайде
+swiperText
+	.on('slideChangeTransitionStart', function () {
+		video.classList.add('change')
+	})
+	.on('slideChangeTransitionEnd', function () {
+		video.classList.remove('change')
+	})
